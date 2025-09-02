bdcf1bf1b247e5a88180f66a0fa723b59f4eb35b
Torrin-DW <torrin.dewree@student.hogent.be>
2025-09-02 23:48:42 +0200

    Add files via upload

--- code/proof of concepts/wireless communication/HC-05/HC-05.ino
+++ code/proof of concepts/wireless communication/HC-05/HC-05.ino
@@ -1,28 +1,33 @@
-#include <SoftwareSerial.h>
-#include <Arduino.h>
+int LED = 4;
+int state;
 
-SoftwareSerial BTSerial(9, 8);
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(9, INPUT);
-  pinMode(8, OUTPUT);
+
+void setup ()
+{
+  pinMode (LED, OUTPUT);
+  //Serial1.begin (9600);
   Serial.begin(9600);
-  BTSerial.begin(9600);
-
+  Serial1.begin(9600);
 }
 
-String message = "";
-String messagebuffer = "";
-void loop() {
-  // put your main code here, to run repeatedly:
-  while (BTSerial.available() > 0){
-    char data = (char) BTSerial.read();
-    if (data == ";") {
-      message = messagebuffer;
-      messagebuffer ="";
-      Serial.print(message);
-      message ="You sent" + message;
-      BTSerial.print(message);
+void loop()
+{
+  if (Serial1.available())
+  {
+    state = Serial1.read();
+  }  
+
+  if (state == '1')
+    {
+      digitalWrite(LED,HIGH);
+      Serial.println("LEd aan");
     }
+  else if (state == '0')
+  {
+    digitalWrite(LED,LOW);
+    Serial.println("Led uit");
   }
+
+  //Serial.println(state);
+  delay(100);
 }
